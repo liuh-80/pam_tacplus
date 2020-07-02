80da79ad91bce024717015c959e851bb5b95219d
Venkatesan Mahalingam <venkatesan_mahalinga@dell.com>
2020-07-02 09:57:28 +0800

    Add support to specify source address for TACACS+

--- support.c
+++ support.c
@@ -37,6 +37,8 @@ char tac_service[64];
 char tac_protocol[64];
 char tac_prompt[64];
 char *__vrfname=NULL;
+char tac_source_ip[64];
+struct addrinfo *tac_source_addr = NULL;
 
 void _pam_log(int err, const char *format,...) {
     char msg[256];
@@ -171,6 +173,44 @@ int tacacs_get_password (pam_handle_t * pamh, int flags
     return PAM_SUCCESS;
 }
 
+/* set source ip address for the outgoing tacacs packets */
+void set_source_ip(const char *tac_source_ip) {
+    /*
+        addrinfo created by getaddrinfo must be released with freeaddrinfo.
+        so source ip address will be stored in following static variables.
+    */
+    static struct addrinfo tac_source_address;
+    static struct sockaddr tac_source_sock_addr;
+    static struct sockaddr_in6 tac_source_sock6_addr;
+
+    struct addrinfo hints, *source_address;
+    int rv;
+
+    /* set the source ip address for the tacacs packets */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((rv = getaddrinfo(tac_source_ip, NULL, &hints,
+                          &source_address)) != 0) {
+        _pam_log(LOG_ERR, "error setting the source ip information");
+    } else {
+        tac_source_addr = &tac_source_address;
+        memcpy(tac_source_addr, source_address, sizeof(struct addrinfo));
+
+        if (source_address->ai_family == AF_INET6) {
+            tac_source_addr->ai_addr = (struct sockaddr *)&(tac_source_sock6_addr);
+            memcpy(tac_source_addr->ai_addr, source_address->ai_addr, sizeof(struct sockaddr_in6));
+        }
+        else {
+            tac_source_addr->ai_addr = &(tac_source_sock_addr);
+            memcpy(tac_source_addr->ai_addr, source_address->ai_addr, sizeof(struct sockaddr));
+        }
+
+        freeaddrinfo(source_address);
+        _pam_log(LOG_DEBUG, "source ip is set");
+    }
+}
+
 int _pam_parse (int argc, const char **argv) {
     int ctrl = 0;
     const char *current_secret = NULL;
@@ -183,6 +223,12 @@ int _pam_parse (int argc, const char **argv) {
     tac_protocol[0] = 0;
     tac_prompt[0] = 0;
     tac_login[0] = 0;
+    tac_source_ip[0] = 0;
+
+    if (tac_source_addr != NULL) {
+        /* reset source address */
+        tac_source_addr = NULL;
+    }
 
     for (ctrl = 0; argc-- > 0; ++argv) {
         if (!strcmp (*argv, "debug")) { /* all */
@@ -274,6 +320,10 @@ int _pam_parse (int argc, const char **argv) {
             }
         } else if(!strncmp(*argv, "vrf=", 4)) {
             __vrfname = strdup(*argv + 4);
+        } else if (!strncmp (*argv, "source_ip=", strlen("source_ip="))) {
+            /* source ip for the packets */
+            strncpy (tac_source_ip, *argv + strlen("source_ip="), sizeof(tac_source_ip));
+            set_source_ip(tac_source_ip);
         } else {
             _pam_log (LOG_WARNING, "unrecognized option: %s", *argv);
         }
@@ -292,8 +342,8 @@ int _pam_parse (int argc, const char **argv) {
         _pam_log(LOG_DEBUG, "tac_protocol='%s'", tac_protocol);
         _pam_log(LOG_DEBUG, "tac_prompt='%s'", tac_prompt);
         _pam_log(LOG_DEBUG, "tac_login='%s'", tac_login);
+        _pam_log(LOG_DEBUG, "tac_source_ip='%s'", tac_source_ip);
     }
 
     return ctrl;
 }    /* _pam_parse */
-
--- support.h
+++ support.h
@@ -37,6 +37,7 @@ extern int tac_srv_no;
 extern char tac_service[64];
 extern char tac_protocol[64];
 extern char tac_prompt[64];
+extern struct addrinfo *tac_source_addr;
 
 int _pam_parse (int, const char **);
 unsigned long _resolve_name (char *);
